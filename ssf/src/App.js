1a3a1ea09c41ddb2cca472f265d28a23466cf29e
Yuna-Kim98 <dbsk4030@gmail.com>
2025-02-09 21:52:42 +0900

    25. 02. 09 commit

--- ssf/src/App.js
+++ ssf/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout.jsx';
 import AdminLogin from './pages/AdminLogin.jsx';
 import AdminMain from './pages/AdminMain.jsx';
 import { AuthProvider } from './auth/AuthContext.js';
 import './sytle/adminlogin.css'
+import ImageList from './commons/ImageList.jsx';
+import axios from 'axios';
+import ProductBlock from './commons/ProductBlock.jsx';
 
 function App() {
+  // const [imgList, setImgList] = useState([]);
+  // const [detailList, setDetailList] = useState([]);
+
+  //   useEffect(() => {
+  //       axios.get("/data/example.json")
+  //           .then(res => {
+  //               setImgList(res.data.images);
+  //               setDetailList(res.data.detail);
+  //               console.log('setImgList --> ', res.data.images);
+  //               console.log('setDetailListt --> ', res.data.detail);
+  //           })
+  //           .catch(err => console.log(err));
+  //   }, []);
+
   return (
     <div className="adminLogin-wrap">
+      {/* <ImageList imgList={imgList} />
+      <ProductBlock detailList={detailList} /> */}
       <AuthProvider>
         <BrowserRouter>
           <Routes>
---
+++ ssf/src/commons/ProductBlock.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { FaStar, FaHeart } from "react-icons/fa";
+import Image from './Image.jsx';
+
+export default function ProductBlock({detailList, ulClassName, liClassName}) {
+    return (
+        <ul className={ulClassName}>
+            { detailList && detailList.map((item) => 
+                <li className={liClassName}>
+                    <div>
+                        <Image img={item.img} alt={item.alt} className='' />
+                    </div>
+                    <div>
+                        <p>{item.brand}</p>
+                        <p>{item.title}</p>
+                        <p>{item.costprice}</p>
+                        <p><span>{item.discount}%</span><span>{item.saleprice}</span></p>
+                        <p>
+                            <span>
+                                <span><FaStar /></span>
+                                <span>{item.star}</span>
+                            </span>
+                            <span>
+                                <span><FaHeart /></span>
+                                <span>{item.like}</span>
+                            </span>
+                        </p>
+                    </div>
+                </li>
+            ) }
+        </ul>
+    );
+}
+
+/**
+ * 호출 시 npm react-icons 설치 필수
+ * 단독으로 쓰이지 않는 것 같아 처음부터 리스트로 컴포넌트 생성. 필요시 분리
+ * 호출하는 곳에서 detailList, ulClassName, liClassName 지정하여 구조분해할당으로 받을 것
+ * 데이터 수정 필요
+**/
